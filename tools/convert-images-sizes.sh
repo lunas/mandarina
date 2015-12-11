f945a0896503b663e3f3f10ae01abcd49d2ca250
lunas <lukasnick@gmail.com>
2015-12-11 12:21:00 +0100

    Add galery/images for 3times

---
+++ tools/convert-images-sizes.sh
@@ -0,0 +1,6 @@
+# to resize all *.JPG files in current folder and save them to the subfolder ./small :
+# in the shell, do:
+for file in *.JPG; do convert $file -resize x180 small/$file; done
+
+# you often need to change the orientation information, otherwise some images are shown 90deg rotated:
+for file in *.JPG; do convert $file -auto-orient $file; done   # could be done in one operation with resize, maybe
