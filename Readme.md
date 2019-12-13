a84091234c38a540440d31de63d38f183b20f396
lunas <lukasnick@fastmail.fm>
2019-12-13 08:13:28 +0100

    Uhu-Anpassungen 12.12.2019
    - Seite amigos.html ganz weg
    - Anpassungen auf home page fuer Uhu Exp.
    - neue Galerieseite fuer Uhu
    - neue Uhubilder

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# To convert images
+
+see /tools/convert-images-sizes.sh
+
+
--- tools/convert-images-sizes.sh
+++ tools/convert-images-sizes.sh
@@ -4,3 +4,4 @@ for file in *.JPG; do convert $file -resize x180 small/$file; done
 
 # you often need to change the orientation information, otherwise some images are shown 90deg rotated:
 for file in *.JPG; do convert $file -auto-orient $file; done   # could be done in one operation with resize, maybe
+
